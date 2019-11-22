04f1aa42375c87b9375c5a3db4b7e2f7abcf3da2
Luc <luc.leray@gmail.com>
2019-11-23 06:35:04 +0800

    fix: Do not log error message when config is missing (#136)
    * Do not log error message when no config is added
    
    * update snapshots
    
    * update snapshots (bis)

--- src/index.js
+++ src/index.js
@@ -297,7 +297,8 @@ function getConfig(macro, filename, source, options) {
 
     if (
       optionsConfig.options === undefined &&
-      fileConfig.options === undefined
+      fileConfig.options === undefined &&
+      fileConfig.error !== undefined
     ) {
       // eslint-disable-next-line no-console
       console.error(
@@ -305,9 +306,7 @@ function getConfig(macro, filename, source, options) {
           `for the macro imported from "${source}. ` +
           `Please see the error thrown for more information.`,
       )
-      if (fileConfig.error !== undefined) {
-        throw fileConfig.error
-      }
+      throw fileConfig.error
     }
 
     if (
